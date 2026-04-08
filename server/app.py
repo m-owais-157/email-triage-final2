594aba67fb23929ac4575841ee98c534666f311d
m-owais-157 <mrowais1514@gmail.com>
2026-04-08 06:42:56 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -1,1 +1,12 @@
+import os
+import uvicorn
 from main import app
+
+
+def main():
+    port = int(os.getenv("PORT", 7860))
+    uvicorn.run(app, host="0.0.0.0", port=port)
+
+
+if __name__ == "__main__":
+    main()
